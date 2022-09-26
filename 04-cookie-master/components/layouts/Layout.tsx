cdcb8458d3faa532143b70eb8695a77c9d4c6892
Jose Quintero <tomark4@gmail.com>
2022-09-26 17:06:29 -0400

    install js-cookie

--- 04-cookie-master/components/layouts/Layout.tsx
+++ 04-cookie-master/components/layouts/Layout.tsx
@@ -1,16 +1,13 @@
-import Head from "next/head";
 import React from "react";
 import Navbar from "../ui/Navbar";
 
 const Layout = ({ children }: any) => {
   return (
     <>
-      <Head>
-        <nav>
-          <Navbar />
-        </nav>
-        <main style={{ padding: "20px 50px" }}>{children}</main>
-      </Head>
+      <nav>
+        <Navbar />
+      </nav>
+      <main style={{ padding: "20px 50px" }}>{children}</main>
     </>
   );
 };
--- 04-cookie-master/pages/theme-change.tsx
+++ 04-cookie-master/pages/theme-change.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layouts/Layout";
+import Cookies from "js-cookie";
 
 const ThemeChangePage = () => {
+  const [currentTheme, setCurrentTheme] = useState("custom");
+
+  const onThemeChange = (e: any) => {
+    setCurrentTheme(e.target.value);
+    localStorage.setItem("@theme", e.target.value);
+    Cookies.set("themeSelected", e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(localStorage.getItem("@theme"));
+  }, []);
+
   return (
     <Layout>
-      <div>ThemeChangePage</div>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>
+              <RadioGroup value={currentTheme} onChange={onThemeChange}>
+                <FormControlLabel
+                  label="Light"
+                  value="light"
+                  control={<Radio />}
+                ></FormControlLabel>
+                <FormControlLabel
+                  label="Dark"
+                  value="dark"
+                  control={<Radio />}
+                ></FormControlLabel>
+                <FormControlLabel
+                  label="Custom"
+                  value="custom"
+                  control={<Radio />}
+                ></FormControlLabel>
+              </RadioGroup>
+            </FormLabel>
+          </FormControl>
+        </CardContent>
+      </Card>
     </Layout>
   );
 };
